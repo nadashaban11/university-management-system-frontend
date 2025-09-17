e1987f8dcbae9e19c55ed698bd7225d0f8d6e9c2
MalakkSherif <malaksherifmohamed@gmail.com>
2025-09-18 00:22:47 +0300

    sidebar, admin dashboard, not found page,  students not implemented ye

---
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.css'
+})
+export class AdminDashboardComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { StudentsComponent } from './students/students.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AdminDashboardComponent,
+        title: 'Admin Dashboard'
+    },
+    {
+        path: 'students',
+        component: StudentsComponent,
+        title: 'Students' 
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent,
+        title: 'Not Found Page'
+    },
+];
