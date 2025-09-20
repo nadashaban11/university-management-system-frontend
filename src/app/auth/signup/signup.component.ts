9f8354c97cf834ffb03f6e4271a6ff92269370bf
Nada Shaban <nadashaban971@gmail.com>
2025-09-20 08:14:47 +0300

    edit main.ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],  // 👈 مهم
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent {
 
   signUpForm: FormGroup;
   submitted: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.signUpForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
-      username: ['', [Validators.required, Validators.pattern(/^\S+$/)]],
-      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/)]],
+      email: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/)
+      ]],
       role: ['', [Validators.required]]
     });
   }
@@ -32,6 +38,18 @@ export class SignupComponent {
     this.submitted = true;
     if (this.signUpForm.invalid) return;
 
-    console.log('Form Values:', this.signUpForm.value);
+    const formData = this.signUpForm.value;
+
+    this.http.post('http://localhost:8000/api/auth/register', formData)
+      .subscribe({
+        next: (res: any) => {
+          console.log('User registered:', res);
+          alert('Registration successful!');
+        },
+        error: (err) => {
+          console.error('Error:', err);
+          alert('Something went wrong during registration.');
+        }
+      });
   }
 }
